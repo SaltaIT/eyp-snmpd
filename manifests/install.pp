1cbd0279941b92bdfbe2995c3beae5b94306c8ee
Jordi Prats <jordi.prats@gmail.com>
2020-02-20 13:13:50 +0100

    path

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,6 +9,7 @@ class snmpd::install inherits snmpd {
     exec { 'fix snmpd create user':
       command => "sed 's/^if.*snmpd[^;]*/if false/' ${snmpd::params::create_user_snmpd_v3} > /usr/local/bin/puppet_net-snmp-create-v3-user",
       unless => 'grep "if false" /usr/local/bin/puppet_net-snmp-create-v3-user',
+      path => '/usr/sbin:/usr/bin:/sbin:/bin',
       require => Package[$snmpd::params::package_name],
     }
   }
