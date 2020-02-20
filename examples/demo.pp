031814730b4b02b93710cd986b64306b650ef6d2
Jordi Prats <jordi.prats@gmail.com>
2020-02-20 17:26:25 +0100

    snmpdv3 walk

--- examples/demo.pp
+++ examples/demo.pp
@@ -4,3 +4,5 @@ snmpd::v3user { 'v3testuser':
   authpass => '1234567890',
   encpass  => '1234567890',
 }
+
+# snmpwalk -v3  -l authPriv -u v3testuser -a SHA -A "1234567890"  -x AES -X "1234567890" 127.0.0.1 system
