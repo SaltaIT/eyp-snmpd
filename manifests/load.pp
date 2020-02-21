0e6c0c7b5568d4cd45b96ff2dbf29650466c5a8d
Jordi Prats <jordi.prats@gmail.com>
2020-02-21 14:05:15 +0100

    lint

--- manifests/load.pp
+++ manifests/load.pp
@@ -2,10 +2,10 @@ class snmpd::load(
                     $load1  = max(2, ceiling(sprintf('%d', ($::processorcount)*2.7))),
                     $load5  = max(4, ceiling(sprintf('%d', ($::processorcount)*2.5)),
                     $load15 = max(6, ceiling(sprintf('%d', ($::processorcount)*2.3))),
-  ) inherits snmpd::params {
+                  ) inherits snmpd::params {
 
   include ::snmpd
-  
+
   concat::fragment { 'eyp-snmpd - load':
     target  => '/etc/snmp/snmpd.conf',
     order   => '02',
