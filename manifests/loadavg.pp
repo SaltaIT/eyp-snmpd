0b619f8812a6b889726b44c1b0aa488ee1d2ae48
Jordi Prats <jordi.prats@gmail.com>
2020-02-21 14:21:57 +0100

    rename

---
+++ manifests/loadavg.pp
@@ -0,0 +1,14 @@
+class snmpd::loadavg (
+  $load1  = '2',
+  $load5  = '4',
+  $load15 = '6',
+  ) inherits snmpd::params{
+
+  include ::snmpd
+
+  concat::fragment { 'eyp-snmpd - load':
+    target  => '/etc/snmp/snmpd.conf',
+    order   => '02',
+    content => template("${module_name}/load.erb"),
+  }
+}
