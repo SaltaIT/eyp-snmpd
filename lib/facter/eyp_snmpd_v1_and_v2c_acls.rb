b09fbeeacc85eb1004a00c5fa88a2e8fe0a4c168
Jordi Prats <jordi.prats@gmail.com>
2020-02-26 14:48:27 +0100

    snmp_user_hosts

--- lib/facter/eyp_snmpd_v1_and_v2c_acls.rb
+++ lib/facter/eyp_snmpd_v1_and_v2c_acls.rb
@@ -10,7 +10,7 @@ unless snmp_users.nil? or snmp_users.empty?
   snmp_acls = {}
 
   for snmp_user in snmp_users.split("\n")
-  	snmp_user_hosts = Facter::Util::Resolution.exec('bash -c \'grep com2sec /etc/snmp/snmpd.conf | grep #{snmp_user} | awk "{ print \$3 }" | sort | uniq\'').to_s.split("\n")
+  	snmp_user_hosts = Facter::Util::Resolution.exec("bash -c 'grep com2sec /etc/snmp/snmpd.conf | grep #{snmp_user} | awk \"{ print $3 }\" | sort | uniq'").to_s.split("\n")
     snmp_user_community = Facter::Util::Resolution.exec('bash -c \'grep com2sec /etc/snmp/snmpd.conf | grep #{snmp_user} | awk "{ print \$4 }" | sort | head -n1\'').to_s
 
     snmp_acls[snmp_user] = { 'hosts' => snmp_user_hosts, 'community' => snmp_user_community }
