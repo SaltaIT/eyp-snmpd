71c30fc73e985899620e3907281bd89b877971d9
Jordi Prats <jordi.prats@gmail.com>
2020-02-27 09:49:57 +0100

    demo

--- examples/auto_acl.pp
+++ examples/auto_acl.pp
@@ -7,8 +7,8 @@ class { 'snmpd::loadavg': }
 # snmpwalk -v3  -l authPriv -u v3testuser -a SHA -A "1234567890"  -x AES -X "1234567890" 127.0.0.1 system
 
 snmpd::acl { 'demoacl':
-  community     => $::eyp_snmpd_acls['demo']['community'],
-  allowed_hosts => $::eyp_snmpd_acls['demo']['hosts'],
+  community     => 'abcdefghjiklmn',
+  allowed_hosts => '1.1.1.1',
 }
 
 snmpd::acl { 'autoacl1':
